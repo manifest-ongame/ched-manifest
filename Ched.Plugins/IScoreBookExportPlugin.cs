d8a45edd94a757b21d21a1528841b6ffbec19bbf
Paralleltree <paralleltree@outlook.com>
2020-10-23 21:43:07 +0900

    Add IScoreBookExportPlugin

---
+++ Ched.Plugins/IScoreBookExportPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Ched.Core;
+
+namespace Ched.Plugins
+{
+    /// <summary>
+    /// 譜面データのエクスポートを行うプラグインを表します。
+    /// </summary>
+    public interface IScoreBookExportPlugin : IPlugin
+    {
+        string FileFilter { get; }
+        void Export(ScoreBook book, TextWriter writer);
+        string GetCustomData();
+        void SetCustomData();
+    }
+}
